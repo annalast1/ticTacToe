c3390f7ff415f3aec474ca0d44feea64fb66768a
annalast1 <166384111+annalast1@users.noreply.github.comm>
2025-05-09 15:58:20 +0100

    Reset button working

--- js-file.js
+++ js-file.js
@@ -1,8 +1,10 @@
+"use strict";
 // set the game up
 const Setup = {
     // board
     board() {
-        return board = Array(9).fill(0); 
+        let board = Array(9).fill(0); 
+        return board;
     },
     // valid move
     addToken(board, cell, symbol)  {
@@ -20,21 +22,23 @@ const playerController = (function () {
         {
             name: 'Player One',
             token: 1,
+            class: 'x'
         }, 
         {
             name: 'Player Two',
-            token: 20
+            token: 20,
+            class: 'circle'
         }
     ];
     
     let activePlayer = {};
 
-    firstMove = () => {
-        first = Math.floor(Math.random() * 2);
+    function firstMove() {
+        const first = Math.floor(Math.random() * 2);
         return activePlayer = players[first];
     } 
 
-    takeTurns = () => {
+    function takeTurns() {
         activePlayer = activePlayer === players[0] ?  players[1] : players[0];
         return activePlayer;
     };
@@ -43,7 +47,7 @@ const playerController = (function () {
 })();
 
 const results = (function () {
-    checkWin = () => {
+    const checkWin = (board) => {
         let combo = [
             [board[0], board[1], board[2]],
             [board[3], board[4], board[5]],
@@ -59,26 +63,24 @@ const results = (function () {
             for (let i = 0; i < combo.length; i++) {
                 if (combo[i][0] == 1 && combo[i][1] == 1 && combo[i][2] == 1) {
                     winText(playerController.players[0].name);
-                    console.log(1);
-                    return;
+                    return true;
                 } else if (combo[i][0] == 20 && combo[i][1] == 20 && combo[i][2] == 20) {
                     winText(playerController.players[1].name);
-                    console.log();
-                    return;
+                    return true;
                 } else {
                     continue;
                 }
             }
         })
-    },
+    };
 
-    checkSpace = (board => {
+    const checkSpace = (board) => {
         if (board.includes(0)) {
             return true;
         } else {
             return false;
         }
-    }),
+    },
 
     winText = (winner) => {
         const winMsg = document.getElementById("win-msg");
@@ -88,55 +90,99 @@ const results = (function () {
         winMsgText.innerText = winner + " wins!";
     }
 
-    drawText = () => {
+    const drawText = () => {
         const winMsg = document.getElementById("win-msg");
         winMsg.classList.add("show");
 
         const winMsgText = document.getElementById("win-msg-text");        
         winMsgText.innerText = "It's a draw!";
     }
+
     return {checkSpace, checkWin, winText, drawText};
 })();
 
+function reset(board) {    
+        const winMsg = document.getElementById("win-msg");
+        winMsg.classList = 'win-msg';
+        const square = document.querySelectorAll(".square");
+        // remove box.child
+        const parent = document.getElementById('box');
+        const child = document.querySelectorAll('.square');
+        child.forEach(square => {
+            parent.removeChild(square);
+        })
+
+        box.classList ='box';
+        display();          
+}  
 
 
-const display = (function () {
+function render(board) {
+    // DOM needed
+    const square = document.querySelectorAll(".square");
+    let i = 0;
+    square.forEach(grid => {
+    // switch if 0 1 20 display '', x, o
+    switch (board[i]) {
+        case 0 :
+            break;
+        case 1 :
+            grid.classList.add('x');
+            break;
+        case 20 :
+            grid.classList.add('circle');
+            break; 
+        }
+        i++;
+    }) 
+}    
+
+function display() {
     let board = Setup.board();
     
     let activePlayer = playerController.firstMove();
+    console.log(activePlayer);
     let symbol = activePlayer.token;
-
-    const square = document.querySelectorAll(".square");
-    square.value = board;
-    let i = 0;
-    square.forEach(cell => {
-        cell.value = board[i];
-        cell.dataset.index = i;
-        i++;
-        cell.addEventListener("click", () => {
-            addMark(cell, activePlayer, {once:true});
-        });  
-    })          
-
-
-    function addMark(cell, activePlayer) {
-        
-        if (!Setup.addToken(board, cell.dataset.index, symbol)) {
-            activePlayer = playerController.takeTurns(activePlayer);
-        }
-        results.checkWin(board);
-        if (!results.checkSpace(board)) {
-            results.drawText();
-        }
+    // add class to board    
+    const box = document.getElementById('box');
+    box.classList.add(activePlayer.class);
+    
+    const resetButton = document.getElementById('reset');
+    resetButton.addEventListener("click", () => {
         console.log(board);
-        activePlayer = playerController.takeTurns(activePlayer);
-        symbol = activePlayer.token;       
+        reset(board);
+    })  
+    // create grid
+    for (let i = 0; i < 9; i++) {    
+        let square = document.createElement("div");
+        square.dataset.index = i;
+        square.classList = 'square';
+        box.appendChild(square);
+        square.addEventListener("click", () => {
+            addMark(board, square);    
+        })        
+    }
+    
+    function addMark(board, cell) {
+    
+        if (Setup.addToken(board, cell.dataset.index, symbol)) {
+            console.log(board); 
+            render(board); 
+            results.checkWin(board);
+            results.checkSpace(board);
+            activePlayer = playerController.takeTurns(activePlayer);
+            box.classList = 'box';
+            box.classList.add(activePlayer.class);
+            symbol = activePlayer.token;     
+        }
         
+        if (!results.checkWin(board) && !results.checkSpace(board) ) {
+            results.drawText();
+        }         
     } 
-})();
+};
 
-// 
+display();
 
-// not allowed pointer not showing
-// render board ()
 // form for players to enter name
+
